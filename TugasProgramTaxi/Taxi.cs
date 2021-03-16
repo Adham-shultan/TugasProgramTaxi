8214a81bfcce24c4eeb0adf9374edd91d00591d5
adham-shultan <adhamshultan@students.amikom.ac.id>
2021-03-16 21:33:39 +0700

    Add project files.

---
+++ TugasProgramTaxi/Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasProgramTaxi
+{
+    class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty) Console.WriteLine("On Duty : Yes", OnDuty);
+            else Console.WriteLine("On Duty : No", OnDuty);
+            Console.WriteLine("Number of Passenger : {0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Jono sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Jono selesai mengantar penumpang", DriverName);
+        }
+    }
+}
